574856b8d5ca55c50f2a5abf0dcbc2c40f67e1c3
Miguel Leite <mikleite@gmail.com>
2023-08-04 10:37:57 +0100

    Recap solved

--- models/Planet.js
+++ models/Planet.js
@@ -2,7 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const planetSchema = new Schema({
-    /* Your Code Goes Here ... */
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    velocity: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    distance: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    wifi: {
+        type: String,
+        enum: ['yes', 'no']
+    }
 }); 
 
 const Planet = mongoose.model('Planet', planetSchema);
